a1d2e06fc3fdd94bae1b2290b4ec4fc88546360e
Mark Bevels <mark.bevels@gmail.com>
2015-07-02 13:49:22 +0100

    Tracking methods added to Game Monitor

--- Assets/Scripts/Game/GameMonitor.cs
+++ Assets/Scripts/Game/GameMonitor.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameMonitor : MonoBehaviour {
+public class GameMonitor : Singleton<GameMonitor> {
 
-	// Use this for initialization
-	void Start () {
-	
+	private int currentLevel = 0;
+
+	private bool canCompleteOrFail { get { return currentLevel != 0; } }			
+	private bool canStartNewLevel { get { return currentLevel == 0; } }		
+
+	public void StartLevel(int levelNumber) {
+		if (!canStartNewLevel) { 
+			Debug.LogError("You are playing level: " + currentLevel.ToString() + ". Finish it before starting new level.");
+			return;
+		}
+		currentLevel = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void CompleteLevel() {
+		if (!canCompleteOrFail) { 
+			Debug.LogError("You can't complete the level before starting it");
+			return;
+		}
+
 	}
+
+	public void FailLevel() {
+		if (!canCompleteOrFail) { 
+			Debug.LogError("You can't fail the level before starting it");
+			return;
+		}
+	}
+
 }
