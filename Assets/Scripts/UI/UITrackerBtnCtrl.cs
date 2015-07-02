31720869e7e37f48d23785285ad0c6290d028a48
Mark Bevels <mark.bevels@gmail.com>
2015-07-02 13:50:31 +0100

    OnPointerClick event added to Tracker Btn Ctrl

--- Assets/Scripts/UI/UITrackerBtnCtrl.cs
+++ Assets/Scripts/UI/UITrackerBtnCtrl.cs
@@ -1,11 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 
-public class UITrackerBtnCtrl : MonoBehaviour {
+
+[RequireComponent(typeof(Button))]
+public class UITrackerBtnCtrl : MonoBehaviour, IPointerClickHandler {
+	
+	public enum TrackingType { None, StartLevel, CompleteLevel, FailLevel }
+	private GameMonitor Monitor { get { return GameMonitor.Instance; } }
 
 	public int levelNumber = 0;
+	public TrackingType trackingType = TrackingType.None;
 
-	void OnClick () {
 
+	public void OnPointerClick (PointerEventData eventData) {
+
+		switch(trackingType) {
+		case TrackingType.StartLevel:
+			Monitor.StartLevel(levelNumber);
+			break;
+
+		case TrackingType.CompleteLevel:
+			Monitor.CompleteLevel();
+			break;
+
+		case TrackingType.FailLevel:
+			Monitor.FailLevel();
+			break;
+
+		case TrackingType.None:
+			Debug.LogError("Tracking Type is not selected", this);
+			break;
+		}
 	}
 }
